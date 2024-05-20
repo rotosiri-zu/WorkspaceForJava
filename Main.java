508ed157b5a882cb7610f70a8ac8ccca0baa5a8d
rotosiri-zu <hongjitufang25@gmail.com>
2024-05-20 22:17:36 +0900

    Java教材第1章 1.3.4 変数の初期化

--- Main.java
+++ Main.java
@@ -10,3 +10,33 @@ public class Main {
     System.out.println(x * x * 3.14);
   }
 }
+
+/* 整数を格納できる4つの型 (byte、short、int、long) */
+byte glasses;
+glasses = 2;
+short age;
+age = 18;
+int salary;
+salary = 152000;
+long worldPeople;
+worldPeople = 6900000000L;
+
+/* 小数を格納できる2つの型 (float、double) */
+float weight;
+weight = 67.5F;
+double height;
+height = 171.2;
+
+/* YESかNOかを格納できるboolean型 */
+boolean isError; isError = true;
+boolean result; result = false;
+
+/* 1文字だけを格納できるchar型、文字列を格納できるString型 */
+char zodiac;
+zodiac = '辰'
+String name;
+name = 'すがわら'
+
+/* 変数の初期化 */
+int age;
+age = 22;
