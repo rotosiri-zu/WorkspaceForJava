669477f526c53d3d7faaa6684b21a71d9bbdc38f
rotosiri-zu <hongjitufang25@gmail.com>
2024-05-24 21:49:54 +0900

    Java教材第1章 2.8 練習問題

--- Main.java
+++ Main.java
@@ -1,42 +1,70 @@
 public class Main {
   public static void main(String[] args) {
-    int age = 20;
-    System.out.println("私の年齢は" + age);
-    age = 31;
-    System.out.println("...いや、本当の年齢は" + age);
+    // int age = 20;
+    // System.out.println("私の年齢は" + age);
+    // age = 31;
+    // System.out.println("...いや、本当の年齢は" + age);
+    // System.out.println("私の好きな記号は二重引用符(\")です");
+
+    // """
+      
+    // """;
+
+    // int a = 5;
+    // int b = 3;
+    // int m = Math.max(a, b);
+    // System.out.println("比較実験:" + a + "と" + b + "とで大きいほうは・・・" + m);
+
+    // String age = "31";
+    // int n = Integer.parseInt(age);
+    // System.out.println("あなたは来年、" + (n + 1) + "歳になりますね。");
+
+    // int r = new java.util.Random().nextInt(90);
+    // System.out.println("あなたはたぶん、" + r + "歳ですね？");
+
+    // System.out.println("あなたの名前を入力してください。");
+    // String name = new java.util.Scanner(System.in).nextLine();
+    // System.out.println("あなたの年齢を入力してください。");
+    // int age = new java.util.Scanner(System.in).nextInt();
+    // System.out.println("ようこそ、" + age + "歳の" + name + "さん");
+
+    int x = 5;
+    int y = 10;
+    String ans = "x+yは" + (x + y);
+    System.out.println(ans);
   }
 }
 
 /* 整数を格納できる4つの型 (byte、short、int、long) */
-byte glasses;
-glasses = 2;
-short age;
-age = 18;
-int salary;
-salary = 152000;
-long worldPeople;
-worldPeople = 6900000000L;
+// byte glasses;
+// glasses = 2;
+// short age;
+// age = 18;
+// int salary;
+// salary = 152000;
+// long worldPeople;
+// worldPeople = 6900000000L;
 
 /* 小数を格納できる2つの型 (float、double) */
-float weight;
-weight = 67.5F;
-double height;
-height = 171.2;
+// float weight;
+// weight = 67.5F;
+// double height;
+// height = 171.2;
 
 /* YESかNOかを格納できるboolean型 */
-boolean isError; isError = true;
-boolean result; result = false;
+// boolean isError; isError = true;
+// boolean result; result = false;
 
 /* 1文字だけを格納できるchar型、文字列を格納できるString型 */
-char zodiac;
-zodiac = '辰'
-String name;
-name = 'すがわら'
+// char zodiac;
+// zodiac = '辰'
+// String name;
+// name = 'すがわら'
 
 /* 変数の初期化 */
-int age;
-age = 22;
+// int age;
+// age = 22;
 
 /* 値を書き換えられたくない場合は finalを変数名の前に付けて定数にする  */
 
-final double PI = 3.14;
+// final double PI = 3.14;
