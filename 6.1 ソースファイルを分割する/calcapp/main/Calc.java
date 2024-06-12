f993b68d02d9fbcf7771db3a14eb6c8d8e1d01e2
rotosiri-zu <hongjitufang25@gmail.com>
2024-06-12 18:20:54 +0900

    clacapp.logics.CalcLogicをインポートする

--- 6.1 ソースファイルを分割する/calcapp/main/Calc.java
+++ 6.1 ソースファイルを分割する/calcapp/main/Calc.java
@@ -1,8 +1,13 @@
+// clacapp.logics.CalcLogicをインポートする
+package calcapp.main;
+// import clacapp.logics.CalcLogic;
+import clacapp.logics.CalcLogics.*;
+
 public class Calc {
   public static void main(String[] args) {
     int a = 10; int b = 2;
     int total = CalcLogic.tasu(a, b);
-    int delta = CalcLogic.hiku(a, b);
+    int delta = calcapp.logics.CalcLogic.hiku(a, b);
     System.out.println("足すと" + total + "、引くと" + delta);
   }
 }
