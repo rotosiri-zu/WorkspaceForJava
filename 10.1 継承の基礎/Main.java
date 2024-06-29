0d063ba1d3d3b01ed35cf61f522cf72d3f6d6d3b
rotosiri-zu <hongjitufang25@gmail.com>
2024-06-29 15:18:07 +0900

    10.1 継承の基礎

---
+++ 10.1 継承の基礎/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+    // HeroとSuperHeroのrun()を呼び出す
+    Hero h = new Hero();
+    h.run();
+    SuperHero sh = new SuperHero();
+    sh.run();
+  }
+}
